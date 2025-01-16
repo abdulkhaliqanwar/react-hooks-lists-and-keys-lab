c0799474fd40bbf29b50afd53afbe1b1acf08d0e
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2025-01-16 12:26:32 +0300

    Done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,21 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* Render ProjectItem components here */}
+        {projects.map((project) => (
+          // For each project, render a ProjectItem and pass props like id, title, and technologies
+          <ProjectItem 
+            key={project.id}       // Unique key prop
+            name={project.name}  // Passing the title of the project
+            about={project.about}  // Passing the description
+            technologies={project.technologies}  // Passing the technologies used
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
 
 export default ProjectList;
